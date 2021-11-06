ef0af047526ca208738055cb5f1d75183c1dadb7
paxxous <itsenveehio@gmail.com>
2021-11-06 12:08:26 -0400

    first commit

---
+++ public_html/script.js
@@ -0,0 +1,13 @@
+/* 
+  What do you want >:( I'me hrer stop poking around the source code y tpou idoit/.
+*/
+
+function restore() {
+  document.getElementById('hometitle').innerHTML = 'Welcome to paxxous';
+}
+
+function clicktitle() {
+  document.getElementById('hometitle').innerHTML = 'Why did you click this what do you want >:(';
+  setTimeout(restore, 1000)
+}
+
